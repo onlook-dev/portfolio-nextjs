bf0a40825b0e0b73006285b77aa29dba08be5245
Kiet Ho <hoakiet98@gmail.com>
2024-03-22 17:59:16 +0000

    Updated site using Onlook

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className='bg-white'>
+      <body className='bg-black text-black'>
         <Header />
         {children}
         <AnalyticsWrapper />
