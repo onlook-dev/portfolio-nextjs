04a9aa663d5d89a4f2161f4719d4e0575456642d
Kiet Ho <hoakiet98@gmail.com>
2024-03-22 19:19:54 +0000

    Update with onlook color

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className='bg-gray-200 text-black'>
+      <body className='bg-gray-200 text-black bg-[#000000]'>
         <Header />
         {children}
         <AnalyticsWrapper />
