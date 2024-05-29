9652cfa34d4b34e95ffdf20c2190f8fdc2b726f1
Kiet Ho <hoakiet98@gmail.com>
2024-05-29 18:02:44 +0000

    Test

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className='bg-gray-200 text-black'>
+      <body className='bg-[#220070] text-black'>
         <Header />
         {children}
         <AnalyticsWrapper />
