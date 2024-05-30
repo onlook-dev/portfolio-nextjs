6f5ff15f00b78959ed84e52da94d5e8315d3fbfd
Daniel Farrell <danielfarrell.r@gmail.com>
2024-05-30 14:14:35 +0000

    Changed Colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className='bg-gray-200 text-black'>
+      <body className='bg-[#0f0f0f] text-black'>
         <Header />
         {children}
         <AnalyticsWrapper />
