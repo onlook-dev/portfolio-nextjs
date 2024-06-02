59b3fbb73e1216b52a5a50a83dab2a629edaf67c
Sanna Jammeh <sj@frend.no>
2024-06-02 17:47:41 +0000

    Test

--- components/Header.tsx
+++ components/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
 
           {/* HEADER NAME */}
           <h1 className="text-primary-white text-nav-primary-heading">
-            <span className={`${poppins} text-8xl font-bold`}>Onlook</span>
+            <span className={`${poppins} text-8xl font-bold`}>Online</span>
           </h1>
 
           {/* NAV LINKS */}
