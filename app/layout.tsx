38d7d972cbe96b246b9007687b2498fe833c92e7
Daniel Farrell <danielfarrell.r@gmail.com>
2024-06-06 22:44:08 +0000

    Daniel's site

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className='bg-gray-200 text-black'>
+      <body className='bg-[#171717] text-black'>
         <Header />
         {children}
         <AnalyticsWrapper />
